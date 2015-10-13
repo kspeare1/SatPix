818bbe1321a105ce725337464473a6f857093417
kspeare1 <kspeare1@hwemail.com>
2015-10-13 09:11:23 -0700

    Committing

---
+++ SatellitePictures/src/SatPix.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class SatPix {
+
+	public static void main(String[] args) throws IOException
+	{
+		boolean[][] booleanArr = fileToBoolArray("satpix.in");
+		int sizeOfLargestPasture;
+		
+		
+		
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("satpix.out")));
+		out.println(sizeOfLargestPasture);
+		out.close();
+		}
+	
+	private static boolean[][] fileToBoolArray(String fileName) throws FileNotFoundException, IOException
+	{
+		//This helper method converts the input file into a 2D array of booleans
+		Scanner scan = new Scanner (new File("satpix.in"));
+		System.out.println("This is not how you do this");
+		int dOne = scan.nextInt("what do we put here");
+	}
+
+	private static int recursivelyMeasureAndMarkPasture(int row, int col, boolean[][] arr)
+	{
+		//This recursive method employs the flood-fill algorithm to
+		//count the size of a single pasture and "mark" it so it is not double-counted
+		return 0;
+	}
+	
+}
